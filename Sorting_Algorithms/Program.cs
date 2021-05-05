65981dfe23bb748c82170a38dcccf8d77afb5236
hussainpatel <hussain.patel@hotmail.com>
2021-05-04 18:34:30 -0700

    Selection Sort Algorithm

--- Sorting_Algorithms/Program.cs
+++ Sorting_Algorithms/Program.cs
@@ -7,6 +7,26 @@ namespace Sorting_Algorithms
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+
+            int[] arr = new int[] { 4, 6, 5, 3, 9, 2, 1, 7, 8, 0 };
+
+            Display(arr);
+            Selection_Sort.SelectionSort(arr);
+
+            Console.WriteLine();
+
+            Display(arr);
+
+            Console.Read();
         }
+
+        public static void Display(int[] arr)
+        {
+            foreach (var item in arr)
+            {
+                Console.Write("{0} ",item);
+            }
+        }        
     }
 }
---
+++ Sorting_Algorithms/Selection_Sort.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sorting_Algorithms
+{
+    public static class Selection_Sort
+    {
+        public static void SelectionSort(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = i+1; j < arr.Length; j++)
+                {
+                    if (arr[j] < arr[i])
+                    {
+                        var tmp = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = tmp;
+                    }
+                }
+            }
+
+        }
+    }
+}
